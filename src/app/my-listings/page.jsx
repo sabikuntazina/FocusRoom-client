dad87a3d6614b5700ec52cfe6a28e5ba24f3d467
sabikuntazina2025 <sabikuntazina@gmail.com>
2026-05-22 16:44:13 +0600

    added jwt for my listing  room

--- src/app/my-listings/page.jsx
+++ src/app/my-listings/page.jsx
@@ -18,13 +18,19 @@ const MyListingsPage = async () => {
   if (!user?.id) {
     return <div>User not logged in</div>;
   }
-  // const {token}= await auth.api.getToken({
-  //   headers: await headers()
-  // })
-  // console.log(token)
+const {token}= await auth.api.getToken({
+    headers: await headers()
+  })
+
 
 const res = await fetch(
   `${process.env.NEXT_PUBLIC_SERVER_URL}/mylistings/${user.id}`, 
+    {
+      headers:{
+        authorization: `Bearer ${token}`
+       
+      }
+    }
 
 
 );
